387b15c7aa27aef0dfe4d7670a05dd5bd3e49f4e
stepski011 <vukbg.stojkovic@gmail.com>
2023-06-24 20:14:57 +0200

    Implemented CHC & rendering details

--- m1-user-interface.py
+++ m1-user-interface.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import subprocess
 import os
 
@@ -6,42 +6,97 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """
-    This function renders the index.html file and passes the count of active sessions to it.
-    :return: Rendered index.html template
-    """
     return render_template('index.html', active_sessions=get_sessions_count().json['count'])
 
 
 @app.route('/new_provisioning_session', methods=['POST'])
 def new_provisioning_session():
-    """
-    This function creates a new provisioning session.
-    :return: JSON response with a success message
-    """
-    subprocess.run([os.path.expanduser('~/rt-5gms-application-function/install/bin/m1-session'), "new-provisioning-session", "-e", "MyAppId", "-a", "MyASPId"])
-    return jsonify(message='✅ Provisioning session created!'), 200
+    provSession = subprocess.run([os.path.expanduser('~/rt-5gms-application-function/install/bin/m1-session'),
+                                          "new-provisioning-session",
+                                          "-e",
+                                          "MyAppId",
+                                          "-a",
+                                          "MyASPId"],
+                                          capture_output=True,
+                                          text=True)
+    retrieveprovSession = provSession.stdout.strip()
+    return jsonify(message=f'✅ {retrieveprovSession}!'), 200
+
+@app.route('/new_prov_session_with_content_hosting_configuration', methods=['POST'])
+def new_prov_session_with_content_hosting_configuration():
+    provSessionWithCHC = subprocess.run([os.path.expanduser('~/rt-5gms-application-function/install/bin/m1-session'),
+                             "new-stream",
+                             "-e",
+                             "MyAppId",
+                             "-a",
+                             "MyASPId",
+                             "-n",
+                             'Big Buck Bunny',
+                             'https://ftp.itec.aau.at/datasets/DASHDataset2014/BigBuckBunny/4sec/',
+                             'BigBuckBunny_4s_onDemand_2014_05_09.mpd'],
+                             capture_output=True,
+                             text=True)
+    retrieveProvSessionWithCHC = provSessionWithCHC.stdout.strip() 
+    return jsonify(message=f'✅{retrieveProvSessionWithCHC}!'), 200
+
+
+@app.route('/get_prov_session_details', methods=['GET'])
+def get_prov_session_details():
+    prov_session_details = subprocess.run(
+        [os.path.expanduser('~/rt-5gms-application-function/install/bin/m1-session'),
+         "list",
+         "-v"],
+        capture_output=True,
+        text=True
+    )
+    prov_session_output = prov_session_details.stdout.strip()
+
+    return jsonify({"details": prov_session_output}), 200
+
+
+                                            
 
 @app.route('/get_provisioning_sessions', methods=['GET'])
 def get_provisioning_sessions():
-    """
-    This function runs a CLI command to get a list of active provisioning sessions, parses the output, and returns a JSON response.
-    :return: JSON response with a list of session IDs
-    """
-    result = subprocess.run([os.path.expanduser('~/rt-5gms-application-function/install/bin/m1-session'), "list"], capture_output=True, text=True)
+    result = subprocess.run([os.path.expanduser('~/rt-5gms-application-function/install/bin/m1-session'),
+                             "list"],
+                             capture_output=True,
+                             text=True)
     session_list = result.stdout.split('\n')
     return jsonify(provisioning_sessions=session_list)
 
 @app.route('/get_sessions_count', methods=['GET'])
 def get_sessions_count():
-    """
-    This function counts the number of active sessions and returns a JSON response.
-    :return: JSON response with the count of active sessions
-    """
-    result = subprocess.run([os.path.expanduser('~/rt-5gms-application-function/install/bin/m1-session'), "list"], capture_output=True, text=True)
+    result = subprocess.run([os.path.expanduser('~/rt-5gms-application-function/install/bin/m1-session'),
+                             "list"],
+                             capture_output=True,
+                             text=True)
     session_list = result.stdout.split('\n')
     session_count = len([x for x in session_list if x])
     return jsonify(count=session_count)
+
+@app.route('/delete_provisioning_session', methods=['POST'])
+def delete_provisioning_session():
+    session_id = request.form.get('prov-session-id', None)
+
+    if not session_id:
+        return jsonify(message="Please enter a session ID."), 400
+
+    result = subprocess.run([os.path.expanduser('~/rt-5gms-application-function/install/bin/m1-session'),
+                             "list"],
+                             capture_output=True,
+                             text=True)
+    session_list = result.stdout.split('\n')
+
+    if session_id not in session_list:
+        return jsonify(message="No such session ID exists."), 400
+
+    subprocess.run([os.path.expanduser('~/rt-5gms-application-function/install/bin/m1-session'),
+                    "del-stream",
+                    "-p",
+                    session_id])
+    return jsonify(message=f"Session {session_id} has been deleted."), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
