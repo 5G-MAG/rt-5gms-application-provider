29111699a90956d6661f7dc96f1ccb6c620f9185
stepski011 <vukbg.stojkovic@gmail.com>
2023-07-04 21:09:30 +0200

    Certificates implemented

--- m1-user-interface.py
+++ m1-user-interface.py
@@ -58,9 +58,9 @@ def delete_provisioning_session_by_id():
 # Create Hosting Configuration from JSON file
 @app.route('/create_chc_from_json', methods=['POST'])
 def create_chc_from_json():
-    print("Received POST request:", request.json)  # Debug statement
+    print("Received POST request:", request.json)
     provisioning_session_id = request.json.get('prov-session-id', None)
-    print("Parsed session ID:", provisioning_session_id)  # Debug statement
+    print("Parsed session ID:", provisioning_session_id)
 
     if not provisioning_session_id:
         return jsonify(message="Please enter a session ID."), 400
@@ -72,8 +72,7 @@ def create_chc_from_json():
                             file_path],
                             capture_output=True,
                             text=True)
-    print("Command output:", result.stdout, "Command error:", result.stderr)  # Debug statement
-
+    print("Command output:", result.stdout, "Command error:", result.stderr)
     if result.returncode == 0:
         return jsonify(message=f"✅ CHC created from JSON for session {provisioning_session_id}."), 200
     else:
@@ -90,6 +89,78 @@ def get_all_provisioning_sessions_details():
                              text=True)
     return jsonify(details=result.stdout), 200
 
+
+# Create CHC with multiple entry points
+@app.route('/chc_multiple_entry_points', methods=['POST'])
+def chc_multiple_entry_points():
+    sessionMEP = subprocess.run([os.path.expanduser('~/rt-5gms-application-function/install/bin/m1-session'),
+                                                    "new-stream",
+                                                    "-e",
+                                                    "MyAppId",
+                                                    "-a",
+                                                    "MyASPId",
+                                                    "-n",
+                                                    'Big Buck Bunny',
+                                                    'http://amssamples.streaming.mediaservices.windows.net/622b189f-ec39-43f2-93a2-201ac4e31ce1/BigBuckBunny.ism/',
+                                                    'manifest(format=mpd-time-csf)',
+                                                    'manifest(format=m3u8-aapl-v3)'],
+                                                    capture_output=True,
+                                                    text=True)
+        
+    retrieveprovSession = sessionMEP.stdout.strip()
+    session_id = re.search(r'\b[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\b', retrieveprovSession)
+    if session_id is not None:
+        return jsonify(session_id=session_id.group(0), message=f'✅ {retrieveprovSession}!'), 200
+    else:
+        return jsonify(message='Failed to extract session ID.'), 500
+    
+
+# Create Server Certificate
+@app.route('/new_certificate', methods=['POST'])
+def new_certificate():
+    session_id = request.json.get('prov-session-id', None)
+
+    if not session_id:
+        return jsonify(message="Please enter a session ID."), 400
+
+    result = subprocess.run([os.path.expanduser('~/rt-5gms-application-function/install/bin/m1-session'),
+                             "new-certificate",
+                             "-p",
+                             session_id],
+                             capture_output=True,
+                             text=True)
+
+    certificate_id = re.search(r'certificate_id=([\w-]+)', result.stdout)
+    if certificate_id is not None:
+        certificate_id = certificate_id.group(1)
+    
+    if result.returncode == 0:
+        return jsonify(message=f"✅ New certificate created for session {session_id}.", certificate_id=certificate_id), 200
+    else:
+        return jsonify(message=f"Failed to create a new certificate for session {session_id}. Error: {result.stderr}"), 500
+
+# Show Server Certificate
+@app.route('/show_certificate', methods=['POST'])
+def show_certificate():
+    session_id = request.json.get('prov-session-id', None)
+    certificate_id = request.json.get('certificate-id', None)
+
+    if not session_id or not certificate_id:
+        return jsonify(message="Please enter a session ID and a certificate ID."), 400
+
+    result = subprocess.run([os.path.expanduser('~/rt-5gms-application-function/install/bin/m1-session'),
+                             "show-certificate",
+                             "-p",
+                             session_id,
+                             "-c",
+                             certificate_id],
+                             capture_output=True,
+                             text=True)
+    
+    if result.returncode == 0:
+        return jsonify(details=result.stdout), 200
+    else:
+        return jsonify(message=f"Failed to show certificate for session {session_id}. Error: {result.stderr}"), 500
 
 
 if __name__ == '__main__':
