8436f02307a0473e626f101ef80c347044fdfb7a
cedric-len <99037764+cedric-len@users.noreply.github.com>
2024-05-24 11:41:09 +0200

    Update qoe-metrics-dashboard/src/app/app.tsx
    Co-authored-by: Jonas Heisterberg <103542043+Persists@users.noreply.github.com>

--- qoe-metrics-dashboard/src/app/app.tsx
+++ qoe-metrics-dashboard/src/app/app.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import NavBar from './components/nav-bar/NavBar';
 
