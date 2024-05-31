224c2c7295ed8e4a7a7dfc9b78b78367afc8d9ab
Cedric Lenßen <cedric.lenssen@gmail.com>
2024-05-31 11:50:53 +0200

    add recharts

--- qoe-metrics-dashboard/src/app/pages/detail-page/DetailPage.tsx
+++ qoe-metrics-dashboard/src/app/pages/detail-page/DetailPage.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+
+const data = [
+  { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
+  { name: 'Page B', uv: 300, pv: 2400, amt: 2400 },
+  { name: 'Page C', uv: 100, pv: 2400, amt: 2400 },
+  { name: 'Page D', uv: 200, pv: 2400, amt: 2400 },
+];
 
 function DetailPage() {
-  return <div>DetailPage</div>;
+  const renderLineChart = (
+    <LineChart width={600} height={300} data={data}>
+      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+      <CartesianGrid stroke="#ccc" />
+      <XAxis dataKey="name" />
+      <YAxis />
+    </LineChart>
+  );
+  return <div>{renderLineChart}</div>;
 }
 
 export default DetailPage;
