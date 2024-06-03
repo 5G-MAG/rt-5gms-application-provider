23e21129bba49337516e8e8e8b6e1c79306ea323
David Schmidt <66205241+KonsumGandalf@users.noreply.github.com>
2024-06-03 18:46:35 +0200

    refactor(express-mock-af): app
    Remove comment

--- express-mock-af/test-express-mock-server/app.js
+++ express-mock-af/test-express-mock-server/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const axios = require('axios');
-const { log } = require('debug'); // install with `npm install axios`
+const { log } = require('debug');
 global.EventSource = require('eventsource');
 
 /**
