be83977d965fef357a048194dff272ec92c39a35
Cedric Lenßen <cedric.lenssen@gmail.com>
2024-06-12 10:23:34 +0200

    fix icon usage

--- qoe-metrics-dashboard/src/app/components/metric-type-icon/metric-type-icon.tsx
+++ qoe-metrics-dashboard/src/app/components/metric-type-icon/metric-type-icon.tsx
@@ -1,6 +1,7 @@
-import { Icon, Tooltip } from '@mui/material';
-import { styled } from '@mui/material/styles';
 import React from 'react';
+
+import { Tooltip } from '@mui/material';
+
 import { metricsTypeInformation } from '../../models/const/metrics/metrics-type-icon.record';
 import { EMetricsType } from '../../models/enums/metrics/metrics-type.enum';
 
@@ -11,15 +12,6 @@ interface MetricTypeIconProps {
     metricType: EMetricsType;
 }
 
-const StyleIcon = styled(Icon)(({ theme }) => ({
-    color: theme.palette.primary.main,
-    fontSize: '1.5rem',
-    padding: '0.5rem',
-    marginInline: '0.5rem',
-    background: theme.palette.background.default,
-    borderRadius: '0.5rem'
-}));
-
 /**
  * Allows to represent a metric based on its metric type by an icon
  *
@@ -29,9 +21,18 @@ const StyleIcon = styled(Icon)(({ theme }) => ({
 export function MetricTypeIcon(props: MetricTypeIconProps) {
     return (
         <Tooltip title={metricsTypeInformation[props.metricType].title}>
-            <StyleIcon style={{ background: metricsTypeInformation[props.metricType].backgroundColor, color: 'white' }}>
-                {metricsTypeInformation[props.metricType].iconName}
-            </StyleIcon>
+            {metricsTypeInformation[props.metricType].icon({
+                style: {
+                    background:
+                        metricsTypeInformation[props.metricType]
+                            .backgroundColor,
+                    color: 'white',
+                    fontSize: '1.5rem',
+                    padding: '0.5rem',
+                    marginInline: '0.5rem',
+                    borderRadius: '0.5rem',
+                },
+            })}
         </Tooltip>
     );
 }
--- qoe-metrics-dashboard/src/app/models/const/metrics/metrics-type-icon.record.tsx
+++ qoe-metrics-dashboard/src/app/models/const/metrics/metrics-type-icon.record.tsx
@@ -1,11 +1,17 @@
+import { ReactJSXElement } from '@emotion/react/types/jsx-namespace';
+import DescriptionIcon from '@mui/icons-material/Description';
+import DownloadingIcon from '@mui/icons-material/Downloading';
+import HttpIcon from '@mui/icons-material/Http';
+import RouterIcon from '@mui/icons-material/Router';
 import { green, lightBlue, orange, purple } from '@mui/material/colors';
+
 import { EMetricsType } from '../../enums/metrics/metrics-type.enum';
 
 interface IMetricsTypeIcon {
     /**
-     * The icon name
+     * The icon
      */
-    iconName: string;
+    icon: (params: any) => ReactJSXElement;
 
     /**
      * The background color of the icon
@@ -23,22 +29,22 @@ interface IMetricsTypeIcon {
  */
 export const metricsTypeInformation: Record<EMetricsType, IMetricsTypeIcon> = {
     [EMetricsType.BUFFER_LEVEL]: {
-        iconName: 'downloading',
+        icon: (params) => <DownloadingIcon {...params}></DownloadingIcon>,
         backgroundColor: green[500],
         title: 'Buffer Level',
     },
     [EMetricsType.HTTP_LIST]: {
-        iconName: 'http',
+        icon: (params) => <HttpIcon {...params}></HttpIcon>,
         backgroundColor: purple[500],
         title: 'Http List',
     },
     [EMetricsType.MPD_INFORMATION]: {
-        iconName: 'spatial_audio',
+        icon: (params) => <DescriptionIcon {...params}></DescriptionIcon>,
         backgroundColor: orange[500],
         title: 'MPD(Media Presentation Description) Information',
     },
     [EMetricsType.REP_SWITCH_LIST]: {
-        iconName: 'router',
+        icon: (params) => <RouterIcon {...params}></RouterIcon>,
         backgroundColor: lightBlue[500],
         title: 'Report Switch List',
     },
