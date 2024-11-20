449078acf2aeef95cf54fb7daeb1970bed9a0e6c
stepski <vukbg.stojkovic@gmail.com>
2024-11-20 12:47:48 +0100

    Refactored UI code and updated Dockerfile

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Application Function as one process
-
-#/rt-5gms-application-function/install/bin/open5gs-msafd  &
-/rt-5gms-application-function/install/bin/open5gs-msafd -c local-msaf.yaml &
-
 # Navigate to the management UI directory
 cd /ui/management-ui/
 
