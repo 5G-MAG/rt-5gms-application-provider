d96e716d3c9161a84178e656751e13662de82896
Erik Gaida <erik.gaida@fokus.fraunhofer.de>
2026-01-30 00:06:52 +0100

    the func __saveConfigSection was writting the header a more then one time this was leading into a configparser.DuplicateSectionError when calling docker compose exec app-ui m1-session configure show after a change like docker compose exec app-ui m1-session configure set m1_port 7775 this was corrupting the /etc/rt-5gms/m1-client.conf

--- python/lib/rt_m1_client/configuration.py
+++ python/lib/rt_m1_client/configuration.py
@@ -185,8 +185,8 @@ class Configuration:
             finally:
                 os.umask(old_umask)
         with open(self.__config_filepath[section], 'w') as cfgout:
+            cfgout.write(f'[{section}]\n')
             for sect in ['DEFAULT'] + self.__config[section].sections():
-                cfgout.write(f'[{section}]\n')
                 for key in self.__config[section][sect]:
                     cfgvalue = self.__config[section].get(sect, key, raw=True)
                     defvalue = self.__default_config[section].get(sect, key, raw=True)
@@ -194,7 +194,7 @@ class Configuration:
                         if cfgvalue == defvalue:
                             cfgout.write('#')
                         cfgout.write(f'{key} = {cfgvalue}\n')
-                cfgout.write('\n')
+            cfgout.write('\n')
 
     def __str__(self):
         '''String representation of the configuration
